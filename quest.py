0d62b0e7bf8fe9281e357f871ba4849978f43d0c
vnikitina7 <vnikitina429@gmail.com>
2022-09-29 12:35:17 +0300

    added slicer.py added quests.py

---
+++ quest.py
@@ -0,0 +1,19 @@
+def go_forward():
+    print('You go forward')
+def go_left():
+    print('You go left')
+def go_right():
+    print('You go right')
+def fight(somenumber):
+    print('Your current hp is (20)')
+    return hp
+hp = 100
+def game_over():
+    print('Game over', 'Veronika')
+
+if __name__ == '__main__':
+    go_forward()
+    go_left()
+    go_right()
+    fight(20)
+    game_over()
---
+++ slicer.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    string_name = 'Yooooouuuuu pppppaaaaaaaassssssseeeeeeddd'
+    corrected_string = string_name.replace('oooo', '')
+    corrected_string = corrected_string.replace('uuuu', '')
+    corrected_string = corrected_string.replace('pppp', '')
+    corrected_string = corrected_string.replace('aaaaaaa', '')
+    corrected_string = corrected_string.replace('sssss', '')
+    corrected_string = corrected_string.replace('eeeee', '')
+    corrected_string = corrected_string.replace('dd', '')
+    print(corrected_string)
